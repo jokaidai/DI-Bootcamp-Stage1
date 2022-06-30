3edbac2a7897d9ab13f46115c7cbefc1c8e4d8bb
gastonkornfeld <gatok37@gmail.com>
2022-06-30 10:55:22 -0400

    correction daily challenge words in a frame

--- Week-2/Day-4/DailyChallenge/script.js
+++ Week-2/Day-4/DailyChallenge/script.js
@@ -8,9 +8,19 @@ Instructions
 3.Console.log the words one per line, in a rectangular frame as seen below.
 */
 
+// a few things to correct First the user should be asked only one time for words separated by comas and then stored in a variable
+
+
 function start(){
 	comaMaker();
 }
+
+// remember you should return a frame of * for the words.
+// try to think of how in every iteration you would add * and spaces in the rigth side and left side
+// depending on the length of every word
+// also you should check first the longer word of the array of words give by the user to understand
+// wich is the size of the frame you will be working to
+// let me know when you redo the exercise i will correct it again.
 
 function comaMaker(){
 	let userWords = [];
