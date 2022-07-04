bb1d6371000396457b6d6f9f657404d0f4ec0cc8
jokaidai <34964251+jokaidai@users.noreply.github.com>
2022-07-04 19:38:43 +0300

    xp mandatory finished (without form)

--- Week-3/Day-2/Exercise-XP/script.js
+++ Week-3/Day-2/Exercise-XP/script.js
@@ -24,7 +24,6 @@ console.log(h1);
 h1.addEventListener("mouseover", function(){
 	h1.style.fontSize = (Math.floor(Math.random() * 101)) + "px";
 });
-
 h1.addEventListener("mouseout", function(){ 
 	h1.style.fontSize = "1.5em";
 });
@@ -40,12 +39,12 @@ h2.addEventListener("mouseout", function(){
 	h2.style.background = "none";
 });
 
-let h3 =document.querySelector("h3");
+let h3 = document.querySelector("h3");
 h3.addEventListener("click", function(){
 	h3.style.display = "none";
 });
 h2.addEventListener("mouseover", function(){
-	h3.style.display='block';
+	h3.style.display ='block';
 });
 
 let btn = document.querySelector("button");
@@ -69,4 +68,85 @@ p2.addEventListener("mouseover", function(){
 p2.addEventListener("mouseout", function(){
 	p2.style.opacity = "1";
 });
-//EX1s
+//EX1
+
+//EX4
+/*
+Instructions:
+
+
+In the JS file:
+
+1.Declare a global variable named allBoldItems.
+
+2.Create a function called getBold_items() that takes no parameter. This function should collect all the bold items inside the paragraph and assign them to the allBoldItems variable.
+
+3.Create a function called highlight() that changes the color of all the bold text to blue.
+
+4.Create a function called return_normal() that changes the color of all the bold text back to black.
+
+5.Call the function highlight() onmouseover (ie. when the mouse pointer is moved onto the paragraph), and the function return_normal() onmouseout (ie. when the mouse pointer is moved out of the paragraph). Look at this example
+*/
+
+let boldList = document.querySelectorAll("p strong");
+let allBoldItems = [];
+
+function getBold_items(){
+	let data;
+	for(let i = 0; i < boldList.length; i++){
+		data = boldList[i].innerText;
+		allBoldItems.push(data);
+	}
+}
+function highlight(){
+	for(let i = 0; i < boldList.length; i++){
+		boldList[i].style.color = "blue";
+	}
+}
+function return_normal(){
+	for(let i = 0; i < boldList.length; i++){
+		boldList[i].style.color = "black";
+	}
+}
+
+let pBold = document.querySelectorAll("article")[1].firstElementChild;
+pBold.addEventListener("mouseover", function(){
+	highlight();
+});
+pBold.addEventListener("mouseout", function(){
+	return_normal();
+});
+
+getBold_items();
+console.log(allBoldItems);
+//EX4
+
+//EX5
+/*
+Instructions:
+
+1.Add many events listeners to one element on your webpage. Use the click, mouseover, mouseout and dblclick events.
+
+2.Each listener should do a different thing, for instance - change position x, change position y, change color, change the font size… and more.
+*/
+
+let icon = document.querySelector("h4");
+
+icon.addEventListener("click", function(){
+	icon.style.fontSize = "36px";
+	icon.style.position = "absolute"
+});
+
+icon.addEventListener("dblclick", function(){
+	icon.style.fontSize = "50px";
+	icon.style.position = "relative"
+});
+
+icon.addEventListener("mouseover", function(){
+	icon.style.backgroundColor = "red";
+});
+
+icon.addEventListener("mouseout", function(){
+	icon.style.backgroundColor = "White";
+});
+//EX5
