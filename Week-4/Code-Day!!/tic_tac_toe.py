1859c7536a9db3c28b77d2a5b15c115b7d34d3f1
jokaidai <34964251+jokaidai@users.noreply.github.com>
2022-07-14 13:25:22 +0300

    input + half chack finnished

--- Week-4/Code-Day!!/tic_tac_toe.py
+++ Week-4/Code-Day!!/tic_tac_toe.py
@@ -1,5 +1,5 @@
-def display_board() -> None:
-    """a function to display the board in the screen"""
+from xmlrpc.client import boolean
+
 
 board = [
     [" ", " ", " "],
@@ -7,22 +7,78 @@ board = [
     [" ", " ", " "]
 ]
 
-content = 0
-row = 0
-print ("TIC-TAC-TOE")
-for i in range(0, 7):
+def display_board() -> None:
+    """a function to display the board in the screen"""
+
+    content = 0
+    row = 0
+    print("\n")
+
+    print ("TIC-TAC-TOE")
+    for i in range(0, 7):
         
-    if i == 0 or i == 6:
-        print("*****************")
+        if i == 0 or i == 6:
+            print("*****************")
             
-    elif i % 2 != 0:
-        print(f"*   {board[row][content]} | {board[row][content + 1]} | {board[row] [content + 2]}   *")
-        row += 1
+        elif i % 2 != 0:
+            print(f"*   {board[row][content]} | {board[row][content + 1]} | {board[row] [content + 2]}   *")
+            row += 1
 
-    elif i % 2 == 0:
-        print("* ----|---|---- *")
+        elif i % 2 == 0:
+            print("* ----|---|---- *")
 
-   
-        
+    print("\n")
 
+
+def player_input(player: str) -> None :
+    """a function to ge the input from the player and draw it on the board"""
+
+    row = int(input("enter row: ")) - 1
+    column = int(input("enter column: ")) - 1  
+    
+    if board[row][column] == " ":
+        board[row][column] = player
+    else:
+        print("This spot is already taken you cheater !!! choose an other one")
+        player_input(player)
+
+    print("\n")
+
+
+def check_win_row(row: int) -> bool:
+    """fucntion to check if someone won by the horizontal"""
+
+    next_case = 0
+    if board[row][next_case] == board[row][next_case + 1] == board[row][next_case + 2] and board[row][next_case] != " ":
+        return True
+
+
+def check_win_col(col: int) -> bool:
+    """fucntion to check if someone won by the vertical"""
+    
+    next_case = 0
+    if board[next_case][col] == board[next_case + 1][col] == board[next_case + 2][col] and board[next_case][col] != " ":
+        return True
+
+def check_win() -> bool:
+    """fucntion that will check each turn if there is a winner """
+    
+    for row in range(0, 3):
+        if check_win_row(row):
+            return True 
+        elif check_win_col(row):
+            return True
+
+    return False
+    
+
+display_board()
+player_input("X")
+print(check_win())
+display_board()
+player_input("X")
+print(check_win())
+display_board()
+player_input("X")
+print(check_win())
 display_board()
