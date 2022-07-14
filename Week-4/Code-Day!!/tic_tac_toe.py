ca2f9640569d9081baf713eea10ca3c06d874ec5
jokaidai <34964251+jokaidai@users.noreply.github.com>
2022-07-14 16:08:26 +0300

    check finnished

--- Week-4/Code-Day!!/tic_tac_toe.py
+++ Week-4/Code-Day!!/tic_tac_toe.py
@@ -7,7 +7,7 @@ board = [
     [" ", " ", " "]
 ]
 
-player = "start"
+player = " "
 # global variable
 
 def display_board() -> None:
@@ -33,7 +33,7 @@ def display_board() -> None:
 def choose_player() -> str: 
     """function to choose who's gonna start the X or the O and then change the player each turn accordingly"""
 
-    if player == "start": # only during first call
+    if player == " ": # only during first call
         
         print("To choose who is starting let's flip a coin ...\n")
         coin = random.randint(1, 2) 
@@ -46,17 +46,16 @@ def choose_player() -> str:
             return "O"
 
     elif player == "X":
-        print("player O turn ...\n")
         return "O"
     
     elif player == "O":
-        print("player X turn ...\n")
         return "X"
 
 
 def player_input(player: str) -> None : 
     """a function to ge the input from the player and draw it on the board"""
 
+    global board
     row = int(input("enter row: ")) - 1
     column = int(input("enter column: ")) - 1  
     
@@ -96,8 +95,18 @@ def check_win_diag() -> bool: #-------------this function is called only in the 
         return True
 
 
+def check_tie() -> bool:
+    """function to check if the game is tie (nobody won)"""
+
+    for row in range(0, 3):
+        for col in range(0, 3):
+            if " " in board[row][col]:
+                return False
+    return True
+
+
 def check_win() -> bool:
-    """fucntion that will check each turn if there is a winner """
+    """fucntion that will check each turn if there is a winner by calling other function for each type of win"""
 
     for row in range(0, 3):
         if check_win_row(row):
@@ -111,6 +120,16 @@ def check_win() -> bool:
     return False
 
 
+def game_over() -> None:
+    """function to send messages if the game is over acoordingly to the result"""
+
+    if check_win():
+        
+        print(f"Player: {player} won !!! ")
+    else:
+        print("Its a tie !!! better luck next time !!!")
+
+
 def play() -> None:
     """main function that handle the entire game session"""
 
@@ -120,13 +139,15 @@ def play() -> None:
     display_board()
     player = choose_player()
  
-    
-    
-
-    player_input(player)
-    display_board()
-    player = choose_player()
-    player_input(player)
-    display_board()
+    while True:
+        
+        player_input(player)
+        display_board()
+        if check_tie() or check_win():
+            break
+        player = choose_player()
+        print(f"Player: {player}  turn ...\n")   
+       
+    game_over()
 
 play()
