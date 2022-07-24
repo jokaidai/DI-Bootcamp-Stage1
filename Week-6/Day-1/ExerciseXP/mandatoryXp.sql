109a0396676ff66af9921b13ff81ea7acfb7263e
jokaidai <34964251+jokaidai@users.noreply.github.com>
2022-07-24 17:30:25 +0300

    mandatory finnished

---
+++ Week-6/Day-1/ExerciseXP/mandatoryXp.sql
@@ -0,0 +1,37 @@
+-- create table items(
+--  item_id serial primary key,
+--  item_type varchar (100) not null,
+--  price int not null
+-- )
+
+-- insert into items(item_type, price)
+-- values
+-- ('Small Desk', 100),
+-- ('Large Desk', 300),
+-- ('Fan', 80);
+
+-- create table customers(
+--  customer_id serial primary key,
+--  first_name varchar (100) not null,
+--  last_name varchar(100) not null
+-- )
+
+-- insert into customers(first_name, last_name)
+-- values
+-- ('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson');
+
+-- select * from items
+
+-- select * from items where price > 80
+
+-- select * from items where price <= 300
+
+-- select  * from customers where last_name  = 'Smith'
+
+-- select  * from customers where last_name  = 'Jones'
+
+-- select  * from customers where first_name  != 'Scott'
