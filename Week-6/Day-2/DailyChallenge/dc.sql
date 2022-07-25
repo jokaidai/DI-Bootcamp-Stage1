ffb45f90e5e74c642f5c0be116aa20616bffa0e6
jokaidai <34964251+jokaidai@users.noreply.github.com>
2022-07-25 19:38:08 +0300

    dc finnished

---
+++ Week-6/Day-2/DailyChallenge/dc.sql
@@ -0,0 +1,47 @@
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- )
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar')
+
+-- SELECT * FROM FirstTab
+
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- )
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL)
+
+
+-- SELECT * FROM SecondTab
+
+-- Q1. What will be the OUTPUT of the following statement?
+-- output: 0 correct
+
+-- SELECT COUNT(*) 
+-- FROM FirstTab AS ft WHERE ft.id NOT IN (SELECT id FROM SecondTab WHERE id IS NULL)
+
+-- Q2. What will be the OUTPUT of the following statement?
+-- output: 2 correct
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+
+-- Q3. What will be the OUTPUT of the following statement?
+-- output: 2 false :( (correct: 0)
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+
+-- Q4. What will be the OUTPUT of the following statement?
+-- output: 2 correct
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
