2809cbefc19e7869a4de010b1d75b42384d1d4b2
jokaidai <34964251+jokaidai@users.noreply.github.com>
2022-07-29 15:56:57 +0300

    mandatory part 1 finnnished

--- Week-6/Code_Day!!!/ExerciseXP.py
+++ Week-6/Code_Day!!!/ExerciseXP.py
@@ -1,4 +1,3 @@
-from msilib import Table
 import psycopg2
 
 HOSTNAME = 'localhost'
@@ -42,6 +41,34 @@ class MenuItem():
             connection.close()
 
 
+    @classmethod
+    def all(cls:object) -> list:
+        """
+        create and handle a query to get a list of all the data inside the database and return it
+        """
+
+        query = f"select  * from {TABLE};"
+        data = cls.exe_query(query)
+        return data
+
+
+    @classmethod
+    def get_by_name(cls:object, requested:str) -> object:
+        """
+        create and handle a query to get a single record(row) depending on the item_name and return it return none if not finded
+        """
+
+        query = f"select distinct item_id from {TABLE} where item_name = '{requested}';"
+        
+        data = cls.exe_query(query)
+        if data == []:
+            return None
+
+        clean_data = int(data[0][0])
+        
+        return MenuItem.item_list[clean_data - 1]
+
+
     def save(self:object):
         """
         create and handle a query to insert into our table in the database
@@ -63,7 +90,7 @@ class MenuItem():
         self.item_price = new_price 
 
 
-    def remove(self:object):
+    def delete(self:object):
         """
         create and handle a query to delete a row in the table ( by id)
         """
@@ -71,10 +98,13 @@ class MenuItem():
         query = f"delete from {TABLE} where item_id = {self.item_id};"
         self.exe_query(query)
         MenuItem.item_list.remove(self)
+    
+ 
 
+            
 
 item = MenuItem('Burger', 35)
-# item.save()
-item.update('Veggie burger', 37)
-# item.remove()
-print(MenuItem.item_list)
+item.save()
+item.update('Veggie Burger', 37)
+print(MenuItem.all())
+print(MenuItem.get_by_name('Veggie Burger'))
